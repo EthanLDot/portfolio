f83fc4b1c18c23d318039313aef3ad69517bd091
EthanLDot <ethanlee2337@gmail.com>
2023-09-29 13:30:59 -0700

    Added descriptions for positions

--- src/pages/experiences/AnheuserBusch.tsx
+++ src/pages/experiences/AnheuserBusch.tsx
@@ -34,15 +34,19 @@ const ExperiencePage: FunctionComponent = () => {
           <div className={styles.scrollableText}>
           <div className={styles.experienceText}>
               <p className={styles.experienceText}>
-                Work in progress. More information soon!
+              In late 2021, I accepted a position for Anheuser Busch's Tech Trainee internship. This allowed me to work with an established team in the headquarters of one of the largest beverage companies in the world.
               </p>
               <p className={styles.experienceText}>&nbsp;</p>
               <p className={styles.experienceText}>
-                Work in progress. More information soon!
+              After some initial work on rewriting Sharepoint documentation for the North American tech teams, I was then tasked to work on a new project for Anheuser that involved finding new ways for IT Support to interact with users and identifying a solution based on user feedback. We took a design thinking approach, and internal focus grouping suggested that our users wanted an online and 24-7 solution more efficient than calling an actual service agent or writing out a ticket request form. Thus, we decided to build a chatbot agent as that would minimize monetary development cost and extra training.
               </p>
               <p className={styles.experienceText}>&nbsp;</p>
               <p className={styles.experienceText}>
-                Work in progress. More information soon!
+              The backbone of our chatbot agent was constructed with Microsoft Power Apps, and I also built a simple interface on top of it using HTML/CSS/JS that would blend in with our IT support landing page. After some initial supervised testing with employees from non-tech departments, I took their feedback and made some improvements before changing the bot logic to make an HTTP request to an existing API endpoint that would assist with service ticket production. Throughout the development of this project, I was in constant communication with my manager, other members of the information technology department, and outside contractors to collaborate on development.
+              </p>
+              <p className={styles.experienceText}>&nbsp;</p>
+              <p className={styles.experienceText}>
+              Early estimates suggested the chatbot would increase resolution rates by 5% and reduce service agent workload by 10%. In fact, the project itself continues to this day; additional integrations with Azure AD, Azure Cognitive Services, and Bot Composer Framework build off of the foundation I helped set. Deployment was just the beginning, the focus now is on continual improvement, user adoption, and integration. I was also honored to be able to live in St. Louis, Missouri while developing my soft skills and technical skills in Anheuser-Busch's fast-paced, yet casual environment.
               </p>
             </div>
           </div>
--- src/pages/experiences/ThomsonReuters.tsx
+++ src/pages/experiences/ThomsonReuters.tsx
@@ -34,15 +34,14 @@ const ExperiencePage: FunctionComponent = () => {
           <div className={styles.scrollableText}>
           <div className={styles.experienceText}>
               <p className={styles.experienceText}>
-                Work in progress. More information soon!
+              In late 2022, I landed a software engineering internship with Thomson Reuters, a multinational conglomerate specializing in various industries. Upon the start of my assignment, I was assigned to the global trade division, which with the help of our corporate partners facilitates billions per year in traded goods.
               </p>
               <p className={styles.experienceText}>&nbsp;</p>
               <p className={styles.experienceText}>
-                Work in progress. More information soon!
-              </p>
+              One of our key team goals was to debut a new SQL analyzer tool, which had to be built from the ground up because we had unique guidelines for best practices. At the beginning of my internship, I was responsible for completing some backend C# code that ran the parser logic for specific SQL rules. This included writing test cases with expected outputs. We then continued by wrapping our code in an ASP.NET Web API app and then focused on developing a functional API with a Read endpoint. Work was done on Swagger UI to simplify testing and present information on the API usage more easily. Finally, a controller was added for this service as well as a basic interface, using components designed by our UI teams and constructed with Angular. I was also put to work on a new Power Automate flow meant to enhance resolution rates by reminding support engineers to respond to technical support escalation tickets in a timely manner, depending on who the last respondent was.              </p>
               <p className={styles.experienceText}>&nbsp;</p>
               <p className={styles.experienceText}>
-                Work in progress. More information soon!
+              The SQL analyzer is still a work in progress. However, during that summer, I still managed to learn about a number of new technologies and tools, while building the front and backend of a corporate web app. In addition, I got to stay in the beautiful Twin Cities metro area and got to network with other interns and employees, including the CEO Steve Hasker.
               </p>
             </div>
           </div>
